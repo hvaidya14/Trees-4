78ea9620ef3a00e9335252ea06200efcf9dbeeed
Himani.Vaidya <85447801+hvaidya14@users.noreply.github.com>
2025-02-19 17:47:40 -0800

    Update sample.java

--- sample.java
+++ sample.java
@@ -5,3 +5,126 @@
 
 
 // Your code here along with comments explaining your approach
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    TreeNode ancestor = null;
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == null || q == null) {
+            return null;
+        }
+        return searchAncestror(root, p, q);
+    }
+    private TreeNode searchAncestror(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (root.left == p && root.right == q) {
+            return root;
+        }
+        if (root.left == q && root.right == p) {
+            return root;
+        }
+        if (root == p && (root.left == q || root.right == q)) {
+            return p;
+        }
+        if (root == q && (root.left == p || root.right == p)) {
+            return q;
+        }
+        if (p.val < root.val && q.val < root.val) {
+            return searchAncestror(root.left, p, q);
+        } else if (p.val > root.val && q.val > root.val) {
+            return searchAncestror(root.right, p, q);
+        } 
+        return root;
+    }
+}
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+        if (p == null || q == null) {
+            return null;
+        }
+        boolean p_found = false;
+        boolean q_found = false;
+
+        p_found = checkifpresent(root, p);
+        q_found = checkifpresent(root, q);
+        if (p_found == true && q_found == true) {
+            return searchAncestor(root, p, q);            
+        } else {
+            System.out.println("here");
+            return null;
+        }
+
+    }
+    private TreeNode searchAncestor(TreeNode root, TreeNode p, TreeNode q) {
+
+        if (root == null ){
+            return null;
+        } 
+        if (root.left == p && root.right == q) {
+            return root;
+        }
+        if (root.left == q && root.right == p) {
+            return root;
+        }
+        if (root == p && (root.left == q || root.right == q)) {
+            return root;
+        }
+        if (root == q && (root.left == p || root.right == p)) {
+            return root;
+        }
+        if (root == p || root == q) {
+            return root;
+        }
+        TreeNode leftsb=searchAncestor(root.left, p, q);
+        TreeNode rightsb=searchAncestor(root.right, p, q);
+        if (leftsb != null && rightsb != null) {
+            return root;
+        }
+
+        if (leftsb != null) {
+            return leftsb;
+        }
+        return rightsb;
+    }
+
+    private boolean checkifpresent (TreeNode root, TreeNode check) {
+        if (root == null) {
+            return false;
+        }
+        if (root.left == check || root.right == check) {
+            return true;
+        }
+        if (root == check) {
+            return true;
+        }
+        return false;
+    }
+}
